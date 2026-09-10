759f4f7bf9a2954ab14e1144b0d059191d0dd4ed
Eric-he123 <1790375105@qq.com>
2026-09-10 21:23:24 +0800

    Create Reverse String.py

---
+++ Reverse String.py
@@ -0,0 +1,14 @@
+# 为了使得空间复杂度为O(1)就只维护left和right即可
+
+class Solution:
+    def reverseString(self, s: list[str]) -> None:
+        """
+        Do not return anything, modify s in-place instead.
+        """
+        left = 0
+        right = len(s) - 1
+        while left < right:
+            s[left], s[right] = s[right], s[left]
+            left += 1
+            right -= 1
+            
